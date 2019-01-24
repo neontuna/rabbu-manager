0afb9c24a9cd772d7d0d5bcee8057acfd7f67ec7
Justin Mullis <justin.mullis@gmail.com>
2019-01-23 20:58:41 -0500

    basic jump start template up and running

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RabbuManager
   class Application < Rails::Application
+    config.application_name = Rails.application.class.parent_name
     config.active_job.queue_adapter = :sidekiq
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2

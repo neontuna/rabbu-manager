0a4886b975f575ebdf0a087091de7b98d1a2d7f7
Justin Mullis <justin.mullis@gmail.com>
2019-01-26 16:40:30 -0500

    Automatically route logged in users to listing index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
   devise_for :users
+
+  authenticated :user do
+    root 'listings#index', as: :authenticated_root
+  end
+
   root to: 'home#index'
   
   resources :listings do

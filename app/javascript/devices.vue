bd623456988c43fc20e492a39c05ae79cbb404fd
Justin Mullis <justin.mullis@gmail.com>
2019-01-27 13:57:01 -0500

    setup new JSONP endpoint on smartthings to avoid CORS errors

--- app/javascript/devices.vue
+++ app/javascript/devices.vue
@@ -1,7 +1,56 @@
 <template>
-  <div id="app">
-    <p>{{ token }}</p>
-  </div>
+<div class="table-responsive">
+  <table class="table table-striped table-bordered table-hover">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Device Name</th>
+        <th>Status</th>
+        <th></th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <% @devices.each do |device| %>
+        <%= content_tag :tr, id: dom_id(device), class: dom_class(device) do %>
+          <td>
+            <!-- Being lazy and using bootstrap classes here for color -->
+            <% case device.hardware_type %>
+            <% when "st_switch" %>
+              <% if device.status == 'on' %>
+                <%= icon('fas', 'lightbulb', class: 'text-warning') %>
+              <% else %>
+                <%= icon('fas', 'lightbulb', class: 'text-muted') %>
+              <% end %>
+            <% when "st_lock" %>
+              <% if device.status == 'locked' %>
+                <%= icon('fas', 'lock', class: 'text-success') %>
+              <% else %>
+                <%= icon('fas', 'lock-open', class: 'text-danger') %>
+              <% end %>
+            <% when "st_thermostat" %>
+              <% if device.status == 'cooling' %>
+                <%= icon('fas', 'temperature-high', class: 'text-primary') %>
+              <% elsif device.status == 'heating' %>
+                <%= icon('fas', 'temperature-high', class: 'text-danger') %>
+              <% else %>
+                <%= icon('fas', 'temperature-high', class: 'text-muted') %>
+              <% end %>
+            <% end %>
+          </td>
+          <td><%= device.display_name %></td>
+          <td><%= device.status %></td>
+          <td>
+            <% if device.hardware_type == "st_thermostat" %>
+              Mode: <%= device.meta["mode"] %> | 
+              Temperature: <%= device.meta["temperature"] %> °F
+            <% end %>
+          </td>
+        <% end %>
+      <% end %>
+    </tbody>
+  </table>
+</div>
 </template>
 
 <script>
@@ -13,13 +62,27 @@ export default {
   data: function () {
     return {
       loading: false,
-      pollInterval: undefined
+      pollInterval: undefined,
+      devices: []
     }
   },
   methods: {
     async pollSmartthings (token, endpoint) {
       this.pollInterval = setInterval(async function () {
-        console.log('sup')
+        try {
+          let result = await $.ajax({
+            type: 'GET',
+            url: `${this.endpoint}/devicesJSONP`,
+            dataType: 'jsonp',
+            headers: {
+              Authorization: `Bearer ${this.token}`
+            }
+          })
+
+          this.devices = result
+        } catch(err) {
+          console.error(err)
+        }
       }.bind(this), 6000)
     }
   }

9f477905aaa36ef09a06effb4b598614d0ac2c58
Justin Mullis <justin.mullis@gmail.com>
2019-01-27 14:35:11 -0500

    device index switched over to Vue component

--- app/javascript/devices.vue
+++ app/javascript/devices.vue
@@ -1,5 +1,9 @@
 <template>
-<div class="table-responsive">
+<div v-if="loading" class="d-flex align-items-center border p-2">
+  <strong>Loading...</strong>
+  <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+</div>
+<div v-else class="table-responsive">
   <table class="table table-striped table-bordered table-hover">
     <thead>
       <tr>
@@ -11,43 +15,23 @@
     </thead>
 
     <tbody>
-      <% @devices.each do |device| %>
-        <%= content_tag :tr, id: dom_id(device), class: dom_class(device) do %>
-          <td>
-            <!-- Being lazy and using bootstrap classes here for color -->
-            <% case device.hardware_type %>
-            <% when "st_switch" %>
-              <% if device.status == 'on' %>
-                <%= icon('fas', 'lightbulb', class: 'text-warning') %>
-              <% else %>
-                <%= icon('fas', 'lightbulb', class: 'text-muted') %>
-              <% end %>
-            <% when "st_lock" %>
-              <% if device.status == 'locked' %>
-                <%= icon('fas', 'lock', class: 'text-success') %>
-              <% else %>
-                <%= icon('fas', 'lock-open', class: 'text-danger') %>
-              <% end %>
-            <% when "st_thermostat" %>
-              <% if device.status == 'cooling' %>
-                <%= icon('fas', 'temperature-high', class: 'text-primary') %>
-              <% elsif device.status == 'heating' %>
-                <%= icon('fas', 'temperature-high', class: 'text-danger') %>
-              <% else %>
-                <%= icon('fas', 'temperature-high', class: 'text-muted') %>
-              <% end %>
-            <% end %>
-          </td>
-          <td><%= device.display_name %></td>
-          <td><%= device.status %></td>
-          <td>
-            <% if device.hardware_type == "st_thermostat" %>
-              Mode: <%= device.meta["mode"] %> | 
-              Temperature: <%= device.meta["temperature"] %> °F
-            <% end %>
-          </td>
-        <% end %>
-      <% end %>
+      <tr v-for="device in devices">
+        <td>
+          <span v-html="deviceIcon(device)"></span>
+        </td>
+        <td>
+          {{ device['name'] }}
+        </td>
+        <td>
+          {{ device['value'] }}
+        </td>
+        <td>
+          <span v-if="device['type'] == 'st_thermostat'">
+            Mode: {{ device.meta['mode'] }} | 
+            Temperature: {{ device.meta['temperature'] }} °F
+          </span>
+        </td>
+      </tr>
     </tbody>
   </table>
 </div>
@@ -58,32 +42,54 @@ export default {
   props: [ 'token', 'endpoint' ],
   mounted () {
     this.pollSmartthings()
+    this.pollInterval = setInterval(this.pollSmartthings, 6000)
   },
   data: function () {
     return {
-      loading: false,
+      loading: true,
       pollInterval: undefined,
       devices: []
     }
   },
   methods: {
-    async pollSmartthings (token, endpoint) {
-      this.pollInterval = setInterval(async function () {
-        try {
-          let result = await $.ajax({
-            type: 'GET',
-            url: `${this.endpoint}/devicesJSONP`,
-            dataType: 'jsonp',
-            headers: {
-              Authorization: `Bearer ${this.token}`
-            }
-          })
+    deviceIcon (device) {
+      switch(device['type']){
+        case 'st_switch':
+          return device['value'] == 'on' ? 
+            `<i class="fas fa-lightbulb text-warning"></i>` :
+            `<i class="fas fa-lightbulb text-muted"></i>`
+        case 'st_lock':
+          return device['value'] == 'locked' ? 
+            `<i class="fas fa-lock text-success"></i>` :
+            `<i class="fas fa-lock-open text-danger"></i>`
+        case 'st_thermostat':
+          switch(device['value']){
+            case 'cooling':
+              return `<i class="fas fa-temperature-high text-primary"></i>`
+            case 'heating':
+              return `<i class="fas fa-temperature-high text-danger"></i>`
+            default:
+              return `<i class="fas fa-temperature-high text-muted"></i>`
+          }
+      }
+    },
+    async pollSmartthings () {
+      try {
+        let result = await $.ajax({
+          type: 'GET',
+          url: `${this.endpoint}/devicesJSONP`,
+          dataType: 'jsonp',
+          headers: {
+            Authorization: `Bearer ${this.token}`
+          }
+        })
 
-          this.devices = result
-        } catch(err) {
-          console.error(err)
-        }
-      }.bind(this), 6000)
+        this.devices = result
+      } catch(err) {
+        console.error(err)
+      } finally {
+        this.loading = false
+      }
     }
   }
 }
